ab9e5cbbf5560b6c082985cfbcd0f12815e90c46
Semetey <boronbaevsemetey@gmail.com>
2023-05-07 09:43:47 +0600

    fix: favorites pages

--- src/screens/Favorites/Favorites.tsx
+++ src/screens/Favorites/Favorites.tsx
@@ -1,17 +1,9 @@
-import { AxiosError } from 'axios'
-import React, { useEffect, useState } from 'react'
-
+import  { useEffect, useState } from 'react'
 import { CardMain } from '@components/index'
 import { favorites } from '@store/favorites/favoriteId'
-import {
-  AnnouncementCardType,
-  CardType,
-  FavoritesAnnounsmentType,
-  FavoritesType,
-} from '@typess/types'
-import { errorHandler } from '@utils/errorHandler'
-
-import api from '../../api/index'
+import { FavoritesType,} from '@typess/types'
+import './favorites.scss'
+import { Typography, Empty, Col } from 'antd'
 
 function Favorites() {
   const [announcement, setAnnouncement] = useState<FavoritesType[]>([])
@@ -27,16 +19,21 @@ function Favorites() {
   console.log(announcement)
 
   return (
-    <div className="announcements">
-      {announcement && announcement[0]
-        ? announcement.map(({ photos, announsment }) => (
-            <CardMain
-              type="main"
-              key={announsment.slug}
-              value={{ ...announsment, photos }}
-            />
-          ))
-        : null}
+    <div className="favorites">
+      <Typography.Title level={3}>Избранные</Typography.Title>
+      <Col className='favorites__list'>
+        {announcement && announcement[0]
+          ? announcement.map(({ photos, announsment }) => (
+              <CardMain
+                type="main"
+                key={announsment.slug}
+                value={{ ...announsment, photos }}
+              />
+            ))
+          : 
+          <Empty description="Избранные отсутствуют" />
+          }
+      </Col>
     </div>
   )
 }
